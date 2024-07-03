464032c9e65ba14e299ec642ecb00de9c10b07e0
bsi97 <qkdtmddlf615@naver.com>
2024-07-04 03:29:42 +0900

    project Settings

---
+++ src/main/java/com/footballyo/FootballyoApplication.java
@@ -0,0 +1,13 @@
+package com.footballyo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballyoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FootballyoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/footballyo/FootballyoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.footballyo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballyoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
